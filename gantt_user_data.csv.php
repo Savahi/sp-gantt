1367db18545a6929f3d67b0d4aef2f4f7a002917
Savahi <11804911+Savahi@users.noreply.github.com>
2020-05-11 01:50:25 +0300

    a backup commit

---
+++ gantt_user_data.csv.php
@@ -0,0 +1,8 @@
+<?php require('auth.php'); if( isAuthRequired() ) { auth(true); } ?>
+Code	f_WebExportLineNumber	Level	Notes
+Груша	1	2	gjgjg
+4	3		sdfsdfsdfsd
+hasdjhsjd111111111111111kjhckjsck1111111	4		!!!!AAAA!!!1BBBB!!
+6	6		Комментарsdfdsfsdий!!z!
+spamrd03	10		Еще комментарий!!!!!!!!
+5	13		null!
---
+++ parameters.js
@@ -0,0 +1,7 @@
+var _inputOnly=false;
+var _lang="ru";
+var _dateDelim="";
+var _timeDelim="";
+var _dateFormat="";
+var _secondsInPixel=13708;
+//var _expandToLevelAtStart=3;
